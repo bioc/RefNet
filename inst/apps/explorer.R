27adb97e1b55d55db16bbc694605605d76035388
Paul Shannon <pshannon@fhcrc.org>
2014-07-18 02:34:47 +0000

    pubmed ids extracted, detection method and interaction type now trimmed down to the parenthetical statement, with the PSI-MI codes removed
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/RefNet@92614 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/apps/explorer.R
+++ inst/apps/explorer.R
@@ -19,12 +19,40 @@ cleanGenes <- function(s)
    toupper(strsplit(genes.raw, " +")[[1]])
 }
 #----------------------------------------------------------------------------------------------------
+simplify.psicquic.strings <- function(tbl)
+{
+   column.names <- colnames(tbl)
+   for(colname in column.names){
+      column <- tbl[, colname]
+      fixed <- sub("\\).*$", "", sub("^.*\\(", "", column))
+      tbl[, colname] <- fixed
+      }
+
+  tbl
+
+} # simplify.psicquic.stings
+#----------------------------------------------------------------------------------------------------
+simplify.pubmed.ids <- function(tbl)
+{
+    column.number <- grep("publicationID", colnames(tbl))
+
+    if(column.number > 0){
+       column.values <- tbl[, column.number]
+       new.column.values <- gsub(".*pubmed:([0-9]+).*", "\\1", column.values)
+       tbl[, column.number] <- new.column.values
+       colnames(tbl)[column.number] <- "pubmedID"
+       }
+
+   tbl
+                          
+} # simplify.psicquic.stings
+#----------------------------------------------------------------------------------------------------
 uiWidgets <- fluidPage(#theme="bootstrap.css", #pageWithSidebar(
-       headerPanel("RefNet"),
+       headerPanel("RefNet (Homo sapiens)"),
        sidebarPanel(width=2,
                     
        selectizeInput(inputId='providers', label='providers',
-                      choices = providers, multiple = TRUE, selected=providers[1]),
+                      choices = providers, multiple = TRUE), # , selected=providers[1]),
                       textInput("genes", "Genes:", initial.genes),
                       submitButton("Find Interactions")
                       ),
@@ -44,17 +72,19 @@ serverFunction <- function(input, output)
            return(empty.data.frame)
        current.providers <- input$providers
        printf("querying for %s from %s", paste(genes, collapse=","), paste(current.providers, collapse=","))
-       tbl <- interactions(refnet, id=genes, provider=current.providers, quiet=FALSE)
+       tbl <- interactions(refnet, id=genes, provider=current.providers, quiet=FALSE, species="9606")
        printf("dim(tbl): %d x %d", nrow(tbl), ncol(tbl))
        if(nrow(tbl) == 0){
           printf("nrow 0, returning empty data frame")
           return(empty.data.frame)
           }
-       #tbl2 <- addGeneInfo(idMapper, tbl)
-       tbl2 <- tbl
-       desired.columns <- c("A.name", "B.name", "type", "publicationID", "A", "B", "detectionMethod", "provider")
+       tbl2 <- addGeneInfo(idMapper, tbl)
+       #tbl2 <- tbl
+       desired.columns <- c("A.name", "B.name", "type",  "detectionMethod", "provider", "publicationID", "A", "B")
        actual.columns <- intersect(desired.columns, colnames(tbl2))
-       unique(tbl2[, actual.columns])
+       tbl3 <- unique(tbl2[, actual.columns])
+       tbl4 <- simplify.psicquic.strings(tbl3)
+       simplify.pubmed.ids(tbl4)
        }) # reactive
 
     output$summary <- renderPrint({
